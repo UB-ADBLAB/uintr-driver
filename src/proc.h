0fb9a09d5889ca3e533130f872dc084065c20a0e
marduk <mike@luxvera.com>
2025-01-05 17:59:20 -0500

    Refactor vector management funcs

--- src/proc.h
+++ src/proc.h
@@ -1,13 +1,17 @@
 #ifndef _INTEL_UINTR_PROC_H
 #define _INTEL_UINTR_PROC_H
 
-#include <linux/types.h>
 #include "protocol.h"
 #include "uintr_types.h"
+#include <linux/types.h>
+
+int uintr_alloc_vector(struct uintr_process_ctx *ctx,
+                       struct uintr_vector_ctx *vec);
+
+int uintr_vector_create(struct uintr_process_ctx *proc, __u32 vector);
+void uintr_vector_free(struct uintr_vector_ctx *vec);
 
 struct uintr_process_ctx *uintr_proc_create(struct task_struct *task);
-int uintr_vector_create(struct uintr_process_ctx *proc, __u32 vector);
 void uintr_proc_destroy(struct uintr_process_ctx *proc);
-void uintr_vector_free(struct uintr_vector_ctx *vec);
 
 #endif
